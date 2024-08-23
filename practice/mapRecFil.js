af5d2d40b50a2ef3298dbe5b5845af8b4be95778
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-08-24 01:39:43 +0530

    Update mapRecFil.js

--- practice/mapRecFil.js
+++ practice/mapRecFil.js
@@ -57,3 +57,49 @@ const output4=arr3.reduce(function(init,val){
 console.log(output2);
 
 console.log(output4);
+
+
+
+
+const users=[
+    {firstName:"vinay",lastName:"malla",age:24},
+    {firstName:"tiru",lastName:"kasi",age:23},
+    {firstName:"krishna",lastName:"sarupu",age:26},
+    {firstName:"rohit",lastName:"janu",age:26}
+];
+
+
+
+const fullName=users.map((x)=>x.firstName+" "+x.lastName);
+
+console.log(fullName);
+
+
+// how many users have particular age;
+const greaterAge=users.reduce(function(init,val){
+    if(init[val.age]){
+init[val.age]=++init[val.age]
+// console.log(init);
+    }else{
+       init[val.age]=1; 
+       console.log(init);
+    }
+    return init;
+},{})
+console.log(greaterAge);
+
+
+const reduceAge=users.reduce(function(init,val){
+    if(val.age>24){
+     init.push(val.firstName);
+
+
+    }
+    return init;
+},[]);
+
+console.log(reduceAge);
+
+const firstLessThirty=users.filter((x)=>x.age>24).map((x)=>x.firstName);
+
+console.log(firstLessThirty);
