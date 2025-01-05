2955ca92c52e5a8ea121d47a724ead3f8cf69df9
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2025-01-05 23:48:39 +0530

    Update mapRecFil.js

--- practice/mapRecFil.js
+++ practice/mapRecFil.js
@@ -78,6 +78,7 @@ console.log(users[0].age);
 console.log(users[3].firstName);
 console.log(users[1].lastName);
 console.log(users[1].age);
+console.log(users[0].lastName);
 const fullName=users.map((x)=>x.firstName+" "+x.lastName);
 
 console.log(fullName);
